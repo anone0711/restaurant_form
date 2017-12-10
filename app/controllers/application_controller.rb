707b182e8ea60c47a93cd5037439aef19da2bd56
Brian <finder.chen@gmail.com>
2017-12-10 20:53:44 +0800

    add restaurants resources for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   private
 
     def authenticate_admin
-      unless current_user.admin?
+      unless current_user.admin?  #在要model/user.rb裡面加入admin?的method current_user是devise提供的helper method  但是controller如何去model裡面找到這個method
         flash[:alert] = "Not allow"
         redirect_to root_path
       end
