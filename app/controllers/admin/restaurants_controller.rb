56c14e5fcedcda67b31267cbd9ece8bec06cba90
Brian <finder.chen@gmail.com>
2017-12-12 22:30:50 +0800

    modify authenticate_admin position

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,4 +1,6 @@
 class Admin::RestaurantsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authenticate_admin
   def index
     @restaurants = Restaurant.page(params[:page]).per(10)
   end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,5 @@
 class RestaurantsController < ApplicationController
   before_action :authenticate_user!
-  before_action :authenticate_admin
   def index
   end
 end
