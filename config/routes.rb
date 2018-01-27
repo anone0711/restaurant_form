1e71cfb831cb70090a9d32584d6ff092000fb5fe
Brian <finder.chen@gmail.com>
2018-01-27 14:42:18 +0800

    setup comment resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
     root "restaurants#index"
   end
 
-  resources :restaurants, only: [:index, :show]
+  resources :restaurants, only: [:index, :show] do
+    resources :comments, only: [:create, :destroy]
+  end
   resources :categories, only: :show
 end
