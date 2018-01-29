01649e877bac0c334428269748676d78055445c4
Brian <finder.chen@gmail.com>
2018-01-29 22:57:33 +0800

    add dependency between models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   mount_uploader :avatar, AvatarUploader
-  has_many :comments
+  has_many :comments, dependent: :restrict_with_error
   has_many :restaurants, through: :comments
   def admin?
     self.role == "admin"
