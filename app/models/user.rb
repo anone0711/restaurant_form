2df374ddc36fd521584120f74b8c96ab39addec6
Brian <finder.chen@gmail.com>
2018-01-31 17:15:13 +0800

    setup self-referential relationships within users table through followships

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@ class User < ApplicationRecord
   has_many :favorited_restaurants, through: :favorites, source: :restaurant
   has_many :likes, dependent: :destroy
   has_many :liked_restaurants, through: :likes, source: :restaurant
+  has_many :followships, dependent: :destroy
+  has_many :followings, through: :followships
   def admin?
     self.role == "admin"
   end
